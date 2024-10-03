6ba4c143639dd35a2a7ccc6d06a1ccaf58f6813e
TheFilimisco <nystepro@gmail.com>
2024-10-03 13:30:13 +0200

    Cambio desde clae

--- src/MainClassRoom.java
+++ src/MainClassRoom.java
@@ -152,35 +152,35 @@ public class MainClassRoom {
             }
         }*/
 
-        Scanner sc = new Scanner(System.in);
-        int edadUser = sc.nextInt();
-        int numeroViajes = sc.nextInt();
-
-        if (edadUser>=25 && edadUser<60){
-            if (numeroViajes==1){
-                System.out.println("Bitllet senzill");
-            } else if (numeroViajes>1 && numeroViajes<10) {
-                System.out.println("T-casual");
-            } else if (numeroViajes>10) {
-                System.out.println("T-usual");
-            }
-        } else if (edadUser<25 && edadUser>16) {
-            if (numeroViajes==1){
-                System.out.println("Bitllet senzill");
-            } else if (numeroViajes>1 && numeroViajes<=10) {
-                System.out.println("T-casual");
-            } else if (numeroViajes>10 && numeroViajes<=30) {
-                System.out.println("T-usual");
-            } else {
-                System.out.println("T-jove");
-            }
-        } else if(edadUser>=60){
-            System.out.println("Targeta Rosa");
-        } else if (edadUser>=4) {
-            System.out.println("T-16");
-        } else {
-            System.out.println("Introducir una edad correcta");
-        }
+//        Scanner sc = new Scanner(System.in);
+//        int edadUser = sc.nextInt();
+//        int numeroViajes = sc.nextInt();
+//
+//        if (edadUser>=25 && edadUser<60){
+//            if (numeroViajes==1){
+//                System.out.println("Bitllet senzill");
+//            } else if (numeroViajes>1 && numeroViajes<10) {
+//                System.out.println("T-casual");
+//            } else if (numeroViajes>10) {
+//                System.out.println("T-usual");
+//            }
+//        } else if (edadUser<25 && edadUser>16) {
+//            if (numeroViajes==1){
+//                System.out.println("Bitllet senzill");
+//            } else if (numeroViajes>1 && numeroViajes<=10) {
+//                System.out.println("T-casual");
+//            } else if (numeroViajes>10 && numeroViajes<=30) {
+//                System.out.println("T-usual");
+//            } else {
+//                System.out.println("T-jove");
+//            }
+//        } else if(edadUser>=60){
+//            System.out.println("Targeta Rosa");
+//        } else if (edadUser>=4) {
+//            System.out.println("T-16");
+//        } else {
+//            System.out.println("Introducir una edad correcta");
+//        }
 
     }
 }
